c107aff90beeb0dc32974297844648e377ab3d1d
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-03 22:43:53 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,8 @@
-const list = document.querySelector('#categories');
-const listItem = document.querySelectorAll('.item');
-console.log('Number of categories:', list.children.length);
+const listEl = document.querySelector('#categories');
+const listItemEl = document.querySelectorAll('.item');
+console.log('Number of categories:', listEl.children.length);
 
-listItem.forEach(category => {
+listItemEl.forEach(category => {
   console.log(`Category: ${category.firstElementChild.textContent}`);
   console.log(`Elements: ${category.lastElementChild.children.length}`);
 });
