e45e81f383d86ced5bf2f0cc53a05aacb46d336b
Kateryna Kleshch <ketrins1993@gmail.com>
2022-08-03 17:34:39 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const changeColorBtnEl = document.querySelector('.widget > .change-color');
+const backgroundColorDescriptorEl = document.querySelector('p > .color');
+
+backgroundColorDescriptorEl.textContent = '#fafafa'; 
+
+const onChangeBackgroundColor = event => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  backgroundColorDescriptorEl.textContent = newColor;
+};
+
+changeColorBtnEl.addEventListener('click', onChangeBackgroundColor);
